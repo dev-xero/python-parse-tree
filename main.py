d6775c2a5d7dcb117eb7bdc47436d25f91cb87ac
dev-xero <elo48858@gmail.com>
2024-04-12 12:56:40 +0100

    feat: Implement proto binary tree

---
+++ main.py
@@ -0,0 +1,46 @@
+class BinaryTree():
+    def __init__(self, root):
+        self.key = root
+        self.leftChild = None
+        self.rightChild = None
+    
+    def insertLeft(self, newNode):
+        subTree = BinaryTree(newNode)      
+        if self.leftChild == None:
+            self.leftChild = subTree
+        else:
+            subTree = BinaryTree(newNode)
+            subTree.leftChild = self.leftChild
+            self.leftChild = subTree
+    
+    def insertRight(self, newNode):
+        subTree = BinaryTree(newNode)
+        if self.rightChild == None:
+            self.rightChild = subTree
+        else:
+            subTree.rightChild = self.rightChild
+            self.rightChild = subTree
+
+    def printNodes(self, node):
+        root = node
+        if root == None:
+            return
+        print(root.key, end=" ")
+        if root.leftChild != None:
+            self.printNodes(root.leftChild)
+        if root.rightChild != None:
+            self.printNodes(root.rightChild)
+
+        
+def main():
+    rootNode = BinaryTree("(")
+    rootNode.insertLeft("+")
+    rootNode.insertLeft("1")
+    rootNode.insertRight(")")
+    rootNode.insertRight("2")
+
+    rootNode.printNodes(rootNode)
+
+
+if __name__ == "__main__":
+    main()
