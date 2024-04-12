04746d94b332532658dfd03ee47c38bbfa5e3a15
dev-xero <elo48858@gmail.com>
2024-04-12 20:07:29 +0100

    feat: Implement model tree method

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ class BinaryTree():
     def setRootKey(self, key):
         self.key = key
     
-    def insertLeft(self, newNode):
-        subTree = BinaryTree(newNode)      
+    def insertLeft(self, subTree):
+        # subTree = BinaryTree(newNode)      
         if self.leftChild == None:
             self.leftChild = subTree
         else:
             subTree = BinaryTree(newNode)
             subTree.leftChild = self.leftChild
             self.leftChild = subTree
+        return subTree
     
-    def insertRight(self, newNode):
-        subTree = BinaryTree(newNode)
+    def insertRight(self, subTree):
+        # subTree = BinaryTree(newNode)
         if self.rightChild == None:
             self.rightChild = subTree
         else:
             subTree.rightChild = self.rightChild
             self.rightChild = subTree
+        return subTree
 
     def printNodes(self, node):
         root = node
@@ -48,9 +50,9 @@ class BinaryTree():
             return treeBuilder
 
         treeBuilder += padding + pointer + node.getRootKey() + '\n'
-        nextPadding = "│  "
+        nextPadding = padding + "│  "
         rightPointer = "└──"
-        leftPointer = "├──" if node.getRightChild() is not None else "└──"
+        leftPointer = "└──" if node.getRightChild() is None else "├──"
         
         treeBuilder = self.modelTree(nextPadding, leftPointer, node.getLeftChild(), treeBuilder)
         treeBuilder = self.modelTree(nextPadding, rightPointer, node.getRightChild(), treeBuilder)
@@ -59,12 +61,31 @@ class BinaryTree():
 
 def main():
     rootNode = BinaryTree("root")
+    node1 = BinaryTree("node1")
+    node2 = BinaryTree("node2")
+    node3 = BinaryTree("node3")
+    node4 = BinaryTree("node4")
+    node5 = BinaryTree("node5")
+    node6 = BinaryTree("node6")
+    node7 = BinaryTree("node7")
+    node8 = BinaryTree("node8")
 
-    rootNode.insertLeft("node1")
-    rootNode.insertRight("node2")
+    rootNode.insertLeft(node1)
+    rootNode.insertRight(node2)
+
+    node1.insertLeft(node3)
+    node1.insertRight(node4)
+
+    node2.insertLeft(node5)
+    node2.insertRight(node6)
+
+    node3.insertLeft(node7)
+    node7.insertLeft(node8)
 
     rootNode.printNodes(rootNode)
-    treeBuild = rootNode.modelTree("\n", "", rootNode)
+    treeBuild = rootNode.modelTree("", "", rootNode)
+    
+    print("\n\n")
     print(treeBuild)
 
 
